cd1aa964fc15e37c8ce5d8766cca1ec755d3ca92
amina-tugusheva <115834261+amina-tugusheva@users.noreply.github.com>
2024-09-30 10:01:43 +0400

    Add files via upload

---
+++ gtaph0/gtaph0/Graph.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace gtaph0
+{
+    class Graph<T>
+    {
+        private Dictionary<T, List<(T, double)>> adjacencyList;
+
+        public Graph()
+        {
+            adjacencyList = new Dictionary<T, List<(T, double)>>();
+        }
+
+        public Graph(string filePath)
+        {
+            adjacencyList = new Dictionary<T, List<(T, double)>>();
+            LoadFromFile(filePath);
+        }
+
+        public Graph(Graph<T> other)
+        {
+            adjacencyList = new Dictionary<T, List<(T, double)>>();
+            foreach (var vertex in other.adjacencyList)
+            {
+                adjacencyList[vertex.Key] = new List<(T, double)>(vertex.Value);
+            }
+        }
+
+        public void AddVertex(T vertex)
+        {
+            if (!adjacencyList.ContainsKey(vertex))
+            {
+                adjacencyList[vertex] = new List<(T, double)>();
+            }
+            else
+            {
+                Console.WriteLine("Вершина уже существует.");
+            }
+        }
+
+        public void AddEdge(T from, T to, double weight = 1.0)
+        {
+            if (!adjacencyList.ContainsKey(from) || !adjacencyList.ContainsKey(to))
+            {
+                Console.WriteLine("Одна или обе вершины не существуют");
+                return;
+            }
+
+            adjacencyList[from].Add((to, weight));
+        }
+
+        public void RemoveVertex(T vertex)
+        {
+            if (!adjacencyList.ContainsKey(vertex))
+            {
+                Console.WriteLine("Вершина не существует");
+                return;
+            }
+
+            adjacencyList.Remove(vertex);
+            foreach (var v in adjacencyList.Keys)
+            {
+                adjacencyList[v].RemoveAll(e => e.Item1.Equals(vertex));
+            }
+        }
+
+        public void RemoveEdge(T from, T to)
+        {
+            if (!adjacencyList.ContainsKey(from))
+            {
+                Console.WriteLine("Первая вершина не существует");
+                return;
+            }
+
+            adjacencyList[from].RemoveAll(e => e.Item1.Equals(to));
+        }
+
+        public void SaveToFile(string filePath)
+        {
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                foreach (var vertex in adjacencyList)
+                {
+                    writer.WriteLine($"{vertex.Key}: {string.Join(", ", vertex.Value)}");
+                }
+            }
+        }
+
+        private void LoadFromFile(string filePath)
+        {
+            using (StreamReader reader = new StreamReader(/*@"C:\Users\tugushevaai\Desktop\gtaph0\graph.txt"*/filePath))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    var parts = line.Split(':');
+                    var vertex = (T)Convert.ChangeType(parts[0].Trim(), typeof(T));
+                    var edges = parts[1].Split(',');
+
+                    foreach (var edge in edges)
+                    {
+                        var edgeParts = edge.Trim().Split(' ');
+                        var toVertex = (T)Convert.ChangeType(edgeParts[0], typeof(T));
+                        double weight = edgeParts.Length > 1 ? Convert.ToDouble(edgeParts[1]) : 1.0;
+
+                        AddVertex(vertex);
+                        AddVertex(toVertex);
+                        AddEdge(vertex, toVertex, weight);
+                    }
+                }
+            }
+        }
+
+        public void PrintAdjacencyList()
+        {
+            foreach (var vertex in adjacencyList)
+            {
+                Console.Write($"{vertex.Key}: ");
+                foreach (var edge in vertex.Value)
+                {
+                    Console.Write($"({edge.Item1}, {edge.Item2}) ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ gtaph0/gtaph0/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace gtaph0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Graph<string> graph = new Graph<string>();
+
+            // Добавление вершин
+            graph.AddVertex("A");
+            graph.AddVertex("B");
+            graph.AddVertex("C");
+
+            // Добавление рёбер
+            graph.AddEdge("A", "B", 2.5);
+            graph.AddEdge("A", "C", 3.0);
+            graph.AddEdge("B", "C", 1.5);
+
+            Console.WriteLine("Вывод списка смежности:");
+            graph.PrintAdjacencyList();
+
+            // Удаление ребра
+            graph.RemoveEdge("A", "C");
+
+            Console.WriteLine("Вывод списка смежности после удаления ребра:");
+            graph.PrintAdjacencyList();
+
+            //добавление ребра 
+            graph.AddEdge("C", "A", 4.0);
+            Console.WriteLine("Вывод списка смежности после добавления нового ребра:");
+            graph.PrintAdjacencyList();
+
+            // Удаление вершины
+            graph.RemoveVertex("C");
+
+            Console.WriteLine("Вывод списка смежности после удаления вершины:");
+            graph.PrintAdjacencyList();
+
+            // Сохранение в файл
+            graph.SaveToFile("graph.txt");
+
+            // Загрузка из файла
+            Graph<string> loadedGraph = new Graph<string>("graph.txt");
+            loadedGraph.PrintAdjacencyList();
+        }
+    }
+}
+/*
+A: B 2.5, C 3.0
+B: A 2.5
+C: A 3.0
+
+graph2
+
+X: Y 1.0
+Y: X 1.0, Z 4.0
+Z: Y 4.0
+
+graph3
+
+P: P 5.0
+Q: R 2.0
+R: Q 2.0
+*/
